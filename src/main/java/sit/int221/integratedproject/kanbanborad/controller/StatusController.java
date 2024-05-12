6209413b1fd09ee3f15a86fc9a218bf7584a33ff
carrynong2 <carrynong2@gmail.com>
2024-05-12 17:48:55 +0700

    add cross origin in status controller

--- src/main/java/sit/int221/integratedproject/kanbanborad/controller/StatusController.java
+++ src/main/java/sit/int221/integratedproject/kanbanborad/controller/StatusController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v2/statuses")
-@CrossOrigin(origins = "http://localhost")
+@CrossOrigin(origins = {"http://ip23kw1.sit.kmutt.ac.th", "http://intproj23.sit.kmutt.ac.th"})
 public class StatusController {
     @Autowired
     private StatusService statusService;
