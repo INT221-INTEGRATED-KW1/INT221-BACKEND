37bf5828531b7e8ecf14121ac5d475e36753e2f2
carrynong2 <carrynong2@gmail.com>
2024-05-21 01:12:37 +0700

    add filter default

--- src/main/java/sit/int221/integratedproject/kanbanborad/repositories/TaskRepository.java
+++ src/main/java/sit/int221/integratedproject/kanbanborad/repositories/TaskRepository.java
@@ -2,12 +2,15 @@ package sit.int221.integratedproject.kanbanborad.repositories;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import sit.int221.integratedproject.kanbanborad.entities.Task;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
     List<Task> findByStatusId(Integer id);
-    List<Task> findByStatusNameIn(List<String> statusNames);
+    @Query("SELECT t FROM Task t WHERE t.status.name IN :statusNames ORDER BY t.id")
+    List<Task> findByStatusNameIn(@Param("statusNames") List<String> statusNames);
     List<Task> findByStatusNameIn(List<String> statusNames, Sort sort);
 }
